b38d59305f50cfebacfdf2804e88271f07a3b500
gilbertmunuotz <128461895+gilbertmunuotz@users.noreply.github.com>
2023-10-23 08:52:17 -0700

    Add files via upload

---
+++ slicing.js
@@ -0,0 +1,5 @@
+const names = ['aaron', 'andrew', 'adrian', 'aston'];
+
+const slicedNames = names.slice(1, 3); // This will create a new array containing elements from index 1 to 3 (excluding index 3)
+
+console.log(slicedNames);
